72045389c6f1385c460e0633824da2432a631df6
steven9587 <steven9587@gmail.com>
2018-05-28 22:19:48 +0800

    20180528Snow

--- src/snowing/Snow.java
+++ src/snowing/Snow.java
@@ -2,29 +2,31 @@ package snowing;
 
 import java.util.Random;
 
-public class Snow extends Thread{
+import snowing.SnowFrame.SnowCanvas;
+
+public class Snow extends Thread {
 	int x;
 	int y;
 	Random random = new Random();
+	private SnowCanvas snowCanvas;
 
-	public Snow() {
+	public Snow(SnowCanvas snowCanvas) {
+		this.snowCanvas=snowCanvas;
 		x = random.nextInt(800);
-		y = 0;
+		y = random.nextInt(600);
 	}
 
 	@Override
 	public void run() {
-		for(int i=0;i<600;i++) {
-			x+=random.nextInt(3)-1;
+		for (int i = y; i < 550; i++) {
+			x += random.nextInt(3) - 1;
 			y+=1;
-			System.out.println(new String(new char[x]).replaceAll("/0", " ")+"雪");
-		}
-		try {
-			sleep(50);
-		} catch (InterruptedException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
+			snowCanvas.repaint();
+			try {
+				sleep(50);
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
 		}
 	}
-	
 }
---
+++ src/snowing/SnowFrame.java
@@ -0,0 +1,45 @@
+package snowing;
+
+import java.awt.Canvas;
+import java.awt.Graphics;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.swing.JFrame;
+
+public class SnowFrame extends JFrame {
+	SnowCanvas canvas = new SnowCanvas();
+
+	public SnowFrame() {
+		setSize(800, 600);
+		setLocation(500, 200);
+		add(canvas);
+		setVisible(true);
+		setDefaultCloseOperation(DISPOSE_ON_CLOSE);
+	}
+
+	public class SnowCanvas extends Canvas {
+		List<Snow> snows = new ArrayList<>();
+
+		public SnowCanvas() {
+			for (int i = 0; i < 100; i++) {
+				Snow snow = new Snow(this);
+				snows.add(snow);
+				snow.start();
+			}
+		}
+
+		@Override
+		public void paint(Graphics g) {
+			for (int i = 0; i < 100; i++) {
+				Snow snow = snows.get(i);
+				g.drawString("雪", snow.x, snow.y);
+			}
+		}
+	}
+
+	public static void main(String[] args) {
+		new SnowFrame();
+	}
+
+}
