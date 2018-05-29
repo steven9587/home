e5a4e1497566710523ac5d842efe7ac9e408f143
steven9587 <steven9587@gmail.com>
2018-05-29 23:11:13 +0800

    snow-final

--- src/snowing/Snow.java
+++ src/snowing/Snow.java
@@ -7,25 +7,28 @@ import snowing.SnowFrame.SnowCanvas;
 public class Snow extends Thread {
 	int x;
 	int y;
-	Random random = new Random();
+	Random randon = new Random();
 	private SnowCanvas snowCanvas;
 
 	public Snow(SnowCanvas snowCanvas) {
 		this.snowCanvas=snowCanvas;
-		x = random.nextInt(800);
-		y = random.nextInt(600);
+		x = randon.nextInt(800);
+		y = randon.nextInt(600);
 	}
 
 	@Override
 	public void run() {
-		for (int i = y; i < 550; i++) {
-			x += random.nextInt(3) - 1;
-			y+=1;
+		while (true) {
+			x += randon.nextInt(3) - 1;
+			y += 1;
 			snowCanvas.repaint();
 			try {
 				sleep(50);
 			} catch (InterruptedException e) {
 				e.printStackTrace();
+			}
+			if(y==600) {
+				y=0;
 			}
 		}
 	}
--- src/snowing/SnowFrame.java
+++ src/snowing/SnowFrame.java
@@ -1,11 +1,13 @@
 package snowing;
 
 import java.awt.Canvas;
+import java.awt.Color;
 import java.awt.Graphics;
 import java.util.ArrayList;
 import java.util.List;
 
 import javax.swing.JFrame;
+import javax.swing.JPanel;
 
 public class SnowFrame extends JFrame {
 	SnowCanvas canvas = new SnowCanvas();
@@ -18,22 +20,24 @@ public class SnowFrame extends JFrame {
 		setDefaultCloseOperation(DISPOSE_ON_CLOSE);
 	}
 
-	public class SnowCanvas extends Canvas {
+	public class SnowCanvas extends JPanel {
 		List<Snow> snows = new ArrayList<>();
-
 		public SnowCanvas() {
-			for (int i = 0; i < 100; i++) {
+			setBackground(Color.black);
+			for(int i=0;i<100;i++) {
 				Snow snow = new Snow(this);
 				snows.add(snow);
 				snow.start();
 			}
 		}
-
+		
 		@Override
-		public void paint(Graphics g) {
-			for (int i = 0; i < 100; i++) {
-				Snow snow = snows.get(i);
-				g.drawString("雪", snow.x, snow.y);
+		protected void paintComponent(Graphics g) {
+			super.paintComponent(g);
+			for(int i=0;i<100;i++) {
+				Snow snow=snows.get(i);
+				g.setColor(Color.white);
+				g.fillOval(snow.x,snow.y , 10,10);
 			}
 		}
 	}
